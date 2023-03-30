e28df481a158c320896aa818148a5703a041d5df
Karan Bagga <74136942+kbagga19@users.noreply.github.com>
2023-03-30 13:27:33 +0530

    Update Payment.jsx

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -33,7 +33,7 @@ console.log(items)
     
     // console.log(JSON.stringify({items}))
     // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:3001/create-payment-intent", {
+    fetch("https://pawsitivelypets-api.onrender.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify( items ),
