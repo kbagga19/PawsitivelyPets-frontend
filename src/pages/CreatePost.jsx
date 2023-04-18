bdd06e12bd17889127e6bee7e48ee7a8657bb252
Karan Bagga <74136942+kbagga19@users.noreply.github.com>
2023-04-18 14:22:40 +0530

    Update CreatePost.jsx

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -53,13 +53,23 @@ export default function CreatePost() {
   }
 
   useEffect(() => {
-    fetch (`https://pawsitivelypets-api.onrender.com/profile/${id}`,{
-      credentials: 'include',
-    }).then(response => {
-      response.json().then(userInfo => {
-          setUsername(userInfo.name);
-      });
-    });
+    fetch ('https://pawsitivelypets-api.onrender.com/profile',{
+       method: "POST",
+        crossDomain: true,
+        headers: {'Content-Type':'application/json',
+                Accept: "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },    
+        body: JSON.stringify({
+            token: window.localStorage.getItem("token")
+        }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+                console.log(data);
+                let usname = data.data.name;
+                setUsername(usname);
+            });
   }, [])
   
 
